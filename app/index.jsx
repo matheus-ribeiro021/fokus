4b08f8e52778b56368ea3c0df601dc3950416921
36129382024.1M <matheusrsaraiva@gmail.com>
2025-09-01 09:53:16 -0300

    alteraçoes matheus

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={style.container}>
-      <Text>matheus</Text>
+      <Image source={require('./foco.png')} />
+      <View style= {style.actions}>
+        <Text style={style.timer}>25:00</Text>
+        <Pressable style={style.button}> 
+          <Text style={style.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+    <View style={style.footer}>
+      <Text style={style.footerText}>
+        Projeto ficticio e sem fins comerciais. Desenvolvido por Aluno.
+      </Text>
     </View>
+    </View>
+
   );
 }
 
@@ -13,7 +25,16 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",  
-      
-  }
+    backgroundColor: "#021123",
+  },
+  actions: {
+    padding: 24,
+    backgroundColor: "#144480",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor:"#144480",
+    alignItems: "center",
+    width: "80%",
+  },
 })
 
